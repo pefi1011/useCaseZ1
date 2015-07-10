67401d5f069a987ef922fe91def9cd38d35859d1
Vassil <vsldimov@gmail.com>
2015-07-10 12:11:55 +0200

    Preprocessing of family ids prepared

--- src/main/scala/ss15Impro3/PreProcessingFamilyId.scala
+++ src/main/scala/ss15Impro3/PreProcessingFamilyId.scala
@@ -1,9 +1,11 @@
 package ss15Impro3
 
+import com.sun.jersey.json.impl.ImplMessages
 import org.apache.flink.api.common.functions.FlatMapFunction
 import org.apache.flink.api.scala.{ExecutionEnvironment, _}
 import org.apache.flink.core.fs.FileSystem.WriteMode
 import org.apache.flink.util.Collector
+import org.apache.flink.api.common.functions.MapFunction
 
 
 
@@ -30,34 +32,56 @@ object PreProcessingFamilyId {
 
     val env = ExecutionEnvironment.getExecutionEnvironment
 
+    val productData: DataSet[String] = env.readTextFile(inputPath + "datainfo.txt")
 
+    val productIdToProductFamily = productData.map(
+      new MapFunction[String, (String, String)]() {
 
-    val userData: DataSet[String] = env.readTextFile(inputPath)
+        def map(in: String): (String, String) = {
+
+          val infos = in.split(";")
+
+          // Remove the hours minutes and seconds of the date
+          (infos(0), infos(1))
+        }
+      }
+    )
+    productIdToProductFamily.writeAsText(outputPath + "/pidToFid", WriteMode.OVERWRITE)
+
+    val userData: DataSet[String] = env.readTextFile(inputPath + "50data.txt")
 
     val userFilterData = userData
-      .filter(_.contains("SALE"))
       .flatMap(
 
-        new FlatMapFunction[String, (String,String)] {
+        new FlatMapFunction[String, (String, String, String, String, String)] {
 
-          def flatMap(in: String, out: Collector[Tuple2[String, String]]) = {
+          def flatMap(in: String, out: Collector[(String, String, String, String, String)]) = {
 
             val info: Array[String] = in.split("\t")
 
             val user = info(1)
-            val products: List[String] = info(3).split(",").toList
+            val session = info(2)
+            val products = info(3)
+            val productsAsArray: Array[String] = info(3).split(",")
+            val transactionType = info(4)
 
-            for (product <- products) {
-              out.collect((user, product))
+            for (prod <- productsAsArray) {
+              out.collect((user, session, products.replace(",", ";"), transactionType, prod))
 
             }
           }
         }).distinct
-      // user grouping
-      .groupBy(0)
-      .reduce((t1,t2) => (t1._1, t1._2 + " " + t2._2)) // To combine all bought products
+      // Join with the product info data set
+      .joinWithHuge(productIdToProductFamily).where(4).equalTo(0)
+      // Get the product family information instead of product id
+      .map(t => (t._1._1, t._1._2, t._1._3, t._1._4, t._2._2))
+      //combine again the products which were bought in one transaction
+      .groupBy(0,1,2,3)
+      .reduce((t1, t2) => (t1._1, t1._2, t1._3, t1._4, t1._5 + ";" + t2._5))
+      // family ids instead
+      .map(t => t._5)
 
-    userFilterData.writeAsText(outputPath + "/userPurchaseHistory" , WriteMode.OVERWRITE)
+    userFilterData.writeAsText(outputPath + "/preProcessingFamilyId" , WriteMode.OVERWRITE)
 
     env.execute("Scala AssociationRule Example")
   }
