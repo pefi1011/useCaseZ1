ce8b0218a1174d9bd6d0cec7d30bd72a0406f3d3
pefi1011 <pefi1011@gmail.com>
2015-07-12 23:51:20 +0200

    basic recommendations per user

--- src/main/scala/ss15Impro3/ArmRecommendations.scala
+++ src/main/scala/ss15Impro3/ArmRecommendations.scala
@@ -1,14 +1,21 @@
 package ss15Impro3
 
-import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.api.common.functions.FlatMapFunction
+import org.apache.flink.api.scala.{ExecutionEnvironment, _}
+import org.apache.flink.core.fs.FileSystem.WriteMode
+import org.apache.flink.util.Collector
+import org.apache.flink.api.common.functions.MapFunction
+
+
 
 object ArmRecommendations {
 
+  var inputArmDataPath = "" //
+  var inputUserDataPath = "" //
+  var outputRecommendationPath = ""
+
   def main(args: Array[String]) {
 
-    var inputArmDataPath = "" //
-    var inputUserDataPath = "" //
-    var outputRecommendationPath = ""
 
     if (args.length < 3) {
       sys.error("inputArmDataPath and inputUserDataPath and outputRecommendationPath console parameters are missing")
@@ -25,11 +32,49 @@ object ArmRecommendations {
     val env = ExecutionEnvironment.getExecutionEnvironment
 
 
+    // Read data
+    val armData = env.readTextFile(inputArmDataPath + "armData.csv")
+    val userPurchaseHistoryData : DataSet[(String, String)] = env.readCsvFile(inputUserDataPath + "userPurchaseHistory.csv")
+
+   val transformedUserPurchaseHistoryData = userPurchaseHistoryData.flatMap(new FlatMapFunction[(String, String), (String, String) ] {
+     override def flatMap(t: (String, String), collector: Collector[(String, String)]): Unit = {
+       val user = t._1
+       val items = t._2
+
+       for (item <- items.split(" ")){
+         collector.collect(user, item)
+       }
+     }
+   })
+
+    val armDataChanged = armData.map(new MapFunction[String, (String, String)] {
+      override def map(t: String): (String, String) = {
+
+        val infos = t.split("\\] => \\[")
+        val left = infos(0).substring(2)
+
+        val index = infos(1).indexOf("]")
+        val right = infos(1).dropRight(index).replace(", ", "")
+        (left, right)
+      }
+    })
+
+    val result = transformedUserPurchaseHistoryData.
+      joinWithHuge(armDataChanged)
+      .where(1)
+      .equalTo(0)
+      .map(t => (t._1, t._2._2))
 
 
+    //productData.writeAsCsv(outputRecommendationPath + "/recommendationsPerUser" , "\n", ",", WriteMode.OVERWRITE)
+    result.writeAsText(outputRecommendationPath + "/recommendationsPerUser", WriteMode.OVERWRITE)
+
+    env.execute("Execute Arm Recommendations")
   }
 
 
 }
 
-class ArmRecommendations {}
+class ArmRecommendations {
+
+}
