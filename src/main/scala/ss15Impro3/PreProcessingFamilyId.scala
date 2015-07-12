615bef763a93b204b444abf2ef3a1a459b52decd
Vassil <vsldimov@gmail.com>
2015-07-12 15:32:05 +0200

    now pre processed data can directrly be put in our algorthm

--- src/main/scala/ss15Impro3/PreProcessingFamilyId.scala
+++ src/main/scala/ss15Impro3/PreProcessingFamilyId.scala
@@ -31,6 +31,8 @@ object PreProcessingFamilyId {
 
     val env = ExecutionEnvironment.getExecutionEnvironment
 
+
+    // Read and process the product information
     val productData: DataSet[String] = env.readTextFile(inputPath + "datainfo.txt")
 
     val productIdToProductFamily = productData.map(
@@ -45,19 +47,19 @@ object PreProcessingFamilyId {
         }
       }
     )
-    productIdToProductFamily.writeAsText(outputPath + "/pidToFid", WriteMode.OVERWRITE)
 
+    // Read an process the transactions
     val userData: DataSet[String] = env.readTextFile(inputPath + "50data.txt")
-
     val userFilterData = userData
       .flatMap(
 
-        new FlatMapFunction[String, (String, String, String, String, String)] {
+        new FlatMapFunction[String, (String, String, String, String, String, String)] {
 
-          def flatMap(in: String, out: Collector[(String, String, String, String, String)]) = {
+          def flatMap(in: String, out: Collector[(String, String, String, String, String, String)]) = {
 
             val info: Array[String] = in.split("\t")
 
+            val day = info(0)
             val user = info(1)
             val session = info(2)
             val products = info(3)
@@ -65,20 +67,21 @@ object PreProcessingFamilyId {
             val transactionType = info(4)
 
             for (prod <- productsAsArray) {
-              out.collect((user, session, products.replace(",", ";"), transactionType, prod))
+              out.collect((day, user, session, products.replace(",", ";"), transactionType, prod))
 
             }
           }
         }).distinct
       // Join with the product info data set
-      .joinWithHuge(productIdToProductFamily).where(4).equalTo(0)
+      .joinWithHuge(productIdToProductFamily).where(5).equalTo(0)
       // Get the product family information instead of product id
-      .map(t => (t._1._1, t._1._2, t._1._3, t._1._4, t._2._2))
+      .map(t => (t._1._1, t._1._2, t._1._3, t._1._4, t._1._5, t._2._2))
       //combine again the products which were bought in one transaction
-      .groupBy(0,1,2,3)
+
+      .groupBy(0,1,2,3,4)
       // family ids instead of
-      .reduce((t1, t2) => (t1._1, t1._2, t1._3, t1._4, t1._5 + ";" + t2._5))
-      .map(t => (t._1, t._2, t._5, t._4))
+      .reduce((t1, t2) => (t1._1, t1._2, t1._3, t1._4, t1._5, t1._6 + ";" + t2._6))
+      .map(t => (t._1, t._2, t._3, t._6, t._5))
 
     userFilterData.writeAsText(outputPath + "/preProcessingFamilyId" , WriteMode.OVERWRITE)
 
